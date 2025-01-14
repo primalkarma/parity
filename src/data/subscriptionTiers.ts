ebe36e0c19b102389c3537affae92b9448882fc4
primalkarma <primalkarma@gmail.com>
2025-01-14 07:30:41 +0000

    finished schema version 1

--- src/data/subscriptionTiers.ts
+++ src/data/subscriptionTiers.ts
@@ -1,6 +1,6 @@
 
 
-// export type TierNames = keyof typeof subscriptionTiers
+export type TierNames = keyof typeof subscriptionTiers
 // export type PaidTierNames = Exclude<TierNames, "Free">
 
 export const subscriptionTiers = {
